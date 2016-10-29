edd6df102c54a0e9f63b4616d3f219431c5a3462
Nikhil Kishore <nk1877@nyu.edu>
2016-10-29 18:24:34 -0400

    #updated_weights

---
+++ README.md
@@ -0,0 +1,8 @@
+# Vizualization Tool.
+
+It is source code for vizualization tool which scores street condition of New York based on factors like number of cars, income, 311 service complaints
+etc.
+
+Here is the link to the tool:
+
+https://nk1877.github.io/StreetCondition/#
--- js/topten.js
+++ js/topten.js
@@ -24,16 +24,19 @@ var yAxis = d3.svg.axis()
     .ticks(10, "%");
 
 
-var compliants = function(wcar,wincome,wstreet){
-    var link1='data/proto1.csv'
-    d3.csv(""+link1,function(calls){
+var compliants = function(zip){
+    var link1=''
+    if(document.getElementById("ctmain").checked){
+            link1 = 'data/callsfiltered2.csv'
+         }
+    else{
+            link1 = 'data/complainfilternta.csv'
+        }  
+        d3.csv(""+link1,function(calls){
             data = calls.map(function(d){
-                street = +(d['rstreet']);
-                income =  +(d['rincome']);
-                car=  +(d['rveh']);
-                census = (d['BoroCT2010'])
-                score = Math.ceil(((wstreet*street)+(wincome*income)+(wcar*car))/3);
-                return {"street":wstreet*street,"income":wincome*income,"car":wcar*car,"score":score,"census":census};
+                call = +(d[''+zip]);
+                ct =  (d['Complaints']);
+                return {"callsmade":call,"Cencus":ct};
             })
             bars(data);
         })
@@ -43,47 +46,82 @@ var compliants = function(wcar,wincome,wstreet){
 
 var bars = function(data){
             var max =d3.max(data,function(d){
-                   return d['score'];
+                   return d['callsmade'];
                    })
                      
              var sorted = data.sort(function(x, y){
-               return d3.descending(x.score, y.score);
+               return d3.descending(x.callsmade, y.callsmade);
             });
             
           
             
-             //console.log(sorted)
+             console.log(sorted)
              var tip = d3.tip()
                       .attr('class', 'd3-tip')
                       .offset([-8, 0])
                       .html(function(d) {
-                        return "<strong>Score based ranks:</strong> <span style='color:red'>" +d['census']+":</span><strong>"+d['score']+'</strong>';
+                        return "<strong>Complain Type:</strong> <span style='color:red'>" +d['Cencus']+":</span><strong>"+d['callsmade']+'</strong>';
                       })
              
              var vis = d3.select("#barcharta");
              var bars = vis.selectAll("rect.bar")
                         .data(sorted)
- 
+             
+            //x.domain(data.map(function(d) { return d.Cencus; }));
+            //y.domain([50, d3.max(sorted, function(d) { return d.callsmade/100; })]);
+            //console.log(x);
+            //console.log(y);
             vis.call(tip);
             
+            var result = $.grep(data, function(e){ return e["callsmade"] == (max); });
+            
+//            vis.append('g')
+//                .attr("class", "x axis")
+//                  .attr("transform", "translate(0," + height + ")")
+//                  .call(xAxis)
+//                  .append("text")
+//                    //.attr("dy", ".71em")
+//                    .style("text-anchor", "end")
+//                    .text("Type of Complaint(Please Hover)")
+//                    .attr('x','180')
+//                    .attr('x','210')
+//                    .attr('y','10');
+//    
+//             vis.append("g")
+//              .attr("class", "y axis")
+//              .call(yAxis)
+//            .append("text")
+//              .attr("transform", "rotate(-90)")
+//              .attr("y", 6)
+//              //.attr("x")
+//              //.attr("dy", ".71em")
+//              .style("text-anchor", "end")
+//              .text("Frequency");
+            
+           
+            //console.log(function(d){return d['Cencus']})
             bars.enter()
                 .append("svg:rect")
                 .attr("class", "bar")
                  .on('mouseover', tip.show)
                 .on('mouseout', tip.hide)
-
+                //.on('mouseclick',mapupdate(function(d){return d['Census'];}));
+            bars.on('click',function(d){
+                console.log(d)
+                mapupdate(d['Cencus']);
+            })
             bars.exit()
                 .remove()
             
             
             
-            var w = 7;
+            var h = 7;
             bars
-                .attr("stroke-width", 5)
-                .attr("width", w)  
+                .attr("stroke-width", 4)
+                .attr("width", h)  
                  .attr("x", function(d, i) {
                         if(i<20){
-                       return (i * 10);
+                       return (i * 12);
                         }
                     })
                 .transition()
@@ -93,12 +131,12 @@ var bars = function(data){
                           {
                     if(i<20){
                     //console.log("top ten:"+max);
-                    return ((d.score/max)*height);
+                    return ((d.callsmade/max)*height);
                     }
                     })
                     .attr("y", function(d, i){
                         if(i<20){
-                    return (height - (((d.score/max)*height)));
+                    return (height - (((d.callsmade/max)*height)));
                         }
                     })
             
@@ -112,10 +150,10 @@ var bars = function(data){
         var svg = d3.select("#svg4")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
-            .attr("preserveAspectRatio", "xMaxYMid")
-            .attr("viewBox", "0 0 280 280")
-            .classed("svg-container", true)
-            .classed("svg-content-responsive", true); 
+            //.attr("preserveAspectRatio", "xMaxYMid")
+            //.attr("viewBox", "0 0 280 280")
+            //.classed("svg-container", true)
+            //.classed("svg-content-responsive", true); 
         
         
    //class to make it responsive
